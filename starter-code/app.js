662781f1c09b3080a46da9edb4cc44a9a3e3f712
Pierre Piot <pierrepiot@MacBook-Pro-de-Alejandro-4.local>
2017-05-03 18:37:58 +0200

    final commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -21,9 +21,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//make bower files accessible
+app.use(express.static(path.join(__dirname, "bower_components")));
 
 app.use('/', index);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
